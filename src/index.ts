a0c6f1355e75a203a5f5ee54900a0221e6b84c4b
Olha Kabatska <kabatskaya.olga@gmail.com>
2023-10-14 21:58:53 +0200

    add classes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-// Вам потрібно створити умовний тип, що служить для встановлення типу, що повертається з функції.Як параметр типу повинен обов'язково виступати функціональний тип.
-
-type FunctionReturnType<T> = T extends (param: () => string) => infer U ? U : undefined;
-
-function funcA(param: () => string): string {
-  return param();
+class Film {
+  constructor(
+    public name: string,
+    public year: number,
+    public rate: number,
+    public oscar: boolean
+  ) { }
 }
 
-const foundetType: FunctionReturnType<typeof funcA> = funcA(() => 'string');
-
-// Вам потрібно створити умовний тип, який приймає функціональний тип з одним параметром(або задовільним) та повертає кортеж, де перше значення - це тип, що функція повертає, а другий - тип її параметру
-
-type getTuples<T> = T extends (param: infer V) => infer U ? [U, V] : undefined;
-
-function funcB(param: number): string {
-  return String(param);
+class Category {
+  films: Film[] = [];
+  constructor(public name: string) { }
 }
-
-let foundetTypeB: getTuples<typeof funcB>; // [string, number]
