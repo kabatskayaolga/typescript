d2a52ec38aaf9646b3ee5af8cdc5d8b271a27fd8
Olha Kabatska <kabatskaya.olga@gmail.com>
2023-10-18 12:36:41 +0200

    add decorators

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,89 @@
-// Попрацюємо з числовим паліндромом.Числовий паліндром — це натуральне число, яке читається зліва направо і справа наліво однаково.Інакше кажучи, відрізняється симетрією запису(розташування цифр), причому число знаків може бути як парним, так і непарним.Але.Паліндром можна отримати як результат операцій над іншими числами.Візьмемо будь - яке натуральне число і складемо його зі зворотним числом, тобто записаним тими самими цифрами, але у зворотному порядку.Проробимо ту саму дію з сумою, що вийшла, і будемо повторювати її доти, доки не утвориться паліндром.Іноді достатньо зробити всього один крок(наприклад, 312 + 213 = 525), але, як правило, потрібно не менше двох.Скажімо, число 96 породжує паліндром 4884 тільки на четвертому кроці.... Вам потрібно написати функцію, яка повертатиме об'єкт, де буде властивістьresultі це буде паліндром, і властивістьsteps— це число викликів до знаходження паліндрома. Для того, щоб перевірити себе використовуйте число 196. Це так зване Lychrel number — число яке немає поліндрому
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-interface IPalindrom {
-  result: number | null;
-  step: number | null;
+import 'reflect-metadata';
+
+// Візьміть декоратор DeprecatedMethod і навчіть його працювати з об'єктом, який вміє приймати причину, через яку його не варто використовувати, і назву методу, яким його можна замінити, якщо це можливо.
+
+// Створіть декоратори MinLength, MaxLength та Email
+
+function MinLength(length: number) {
+  return function <T extends {}>(target: T, propertyKey: string): void | any {
+    let value: string;
+    Reflect.defineProperty(target, propertyKey, {
+      get() {
+        return value;
+      },
+      set(val: string) {
+        if (val.length < length) throw new Error(`Number of symbols can't less than ${length}`);
+        value = val;
+      },
+    });
+  };
 }
 
-function isPalindrom(number: string): boolean {
-  for (let index = 0; index < number.length / 2; index++) {
-    if (number[index] !== number[number.length - 1 - index]) {
-      return false;
-    }
+function MaxLength(length: number) {
+  return function <T extends {}>(target: T, propertyKey: string): void | any {
+    let value: string;
+
+    Reflect.defineProperty(target, propertyKey, {
+      get() {
+        return value;
+      },
+      set(val: string) {
+        if (val.length > length) throw new Error(`Number of symbols can't less than ${length}`);
+        value = val;
+      },
+    });
+  };
+}
+
+function Email<T extends {}>(target: T, propertyKey: string | symbol): void | any {
+  let value: string;
+  Reflect.defineProperty(target, propertyKey, {
+    get() {
+      return value;
+      // return value;
+    },
+    set(val: string) {
+      if (!/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/.test(val)) throw new Error('Email is not valid');
+      value = val;
+    },
+  });
+}
+
+function DeprecatedMethod(reason: string, replacedMethod?: string) {
+  return function <T, A extends any[], R>(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    const originalMethod = target.setEmail;
+
+    descriptor.value = function (this: T, ...args: A): R {
+      console.log(`Don't use this method because ${reason}${replacedMethod ? ', use ' + replacedMethod : ''}`);
+      return originalMethod.apply(this, args);
+    };
+  };
+}
+
+class Validation {
+  @Email
+  @MinLength(55)
+  @MaxLength(30)
+  email: string;
+
+  constructor(email: string) {
+    this.email = email;
   }
-  return true;
-}
 
-function getPalindrom(number: number, step = 1): IPalindrom {
-  if (number === 196) return { result: null, step: null };
-
-  const reverseNumber = +String(number).split('').reverse().join('');
-  const sum = number + reverseNumber;
-  return isPalindrom(String(sum)) ? { result: sum, step: step } : getPalindrom(sum, ++step);
-}
-
-getPalindrom(96);
-getPalindrom(312);
-getPalindrom(54);
-getPalindrom(45454554);
-getPalindrom(196);
-
-// Напишіть функцію, яка приймає масив унікальних елементів і генерує всі можливі перестановки цього масиву.Використовуйте рекурсію для знаходження всіх перестановок.Наприклад, якщо вхідний масив[1, 2, 3], функція має повернути масив, що містить[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]і[3, 2, 1].
-
-function uniqueGenerator(inputArr: number[]): number[][] {
-  const result: number[][] = [];
-
-  function generate(workingArray: number[], generatedArray: number[] = []): void {
-    if (workingArray.length) {
-      workingArray.forEach((item, i) => {
-        generate([...workingArray.slice(0, i), ...workingArray.slice(i + 1)], [...generatedArray, item]);
-      });
-    } else {
-      result.push(generatedArray);
-    }
+  @DeprecatedMethod('this Method is Old', 'setEmail')
+  setEmailDeprecated(value: string): void {
+    this.email = value;
   }
 
-  generate(inputArr);
-
-  return result;
+  setEmail(value: string): void {
+    this.email = value;
+  }
 }
 
-uniqueGenerator([1, 2, 3]);
+const form = new Validation('mailxzsadff.d');
+form.email;
+form.setEmailDeprecated('ddd');
+// console.log(form.email);
