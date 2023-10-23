459a11975d48a4be8c17f8ba7b76754c90be605d
Olha Kabatska <kabatskaya.olga@gmail.com>
2023-10-23 12:37:49 +0200

    oop

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,136 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// Вам необхідно написати додатокTodo list. У списку нотаток повинні бути методи для додавання нового запису, видалення, редагування та отримання повної інформації про нотатку за ідентифікатором, а так само отримання списку всіх нотаток. Крім цього, у користувача має бути можливість позначити нотаток, як виконаний, і отримання інформації про те, скільки всього нотаток у списку і скільки залишилося невиконаними. Нотатки не повинні бути порожніми.
 
-// import 'reflect-metadata';
+// Кожний нотаток має назву, зміст, дату створення і редагування та статус. Нотатки бувають двох типів. Дефолтні та такі, які вимагають підтвердження при ридагуванні.
 
-// Візьміть декоратор DeprecatedMethod і навчіть його працювати з об'єктом, який вміє приймати причину, через яку його не варто використовувати, і назву методу, яким його можна замінити, якщо це можливо.
+// Окремо необхідно розширити поведінку списку та додати можливість пошуку нотатка за ім'ям або змістом.
+// Також окремо необхідно розширити список можливістю сортування нотаток за статусом або часом створення.
 
-// Створіть декоратори MinLength, MaxLength та Email
-
-function Compose(lengthMin: number, lengthMax: number) {
-  const minLegth = MinLength(lengthMin);
-  const maxLength = MaxLength(lengthMax);
-  const email = Email;
-
-  return function (target: any, key: string) {
-    maxLength(target, key);
-    minLegth(target, key);
-    email(target, key);
-  };
+enum Sort {
+  STATUS = 'status',
+  DATE = 'date',
 }
 
-function MinLength(length: number) {
-  return function <T extends {}>(target: T, propertyKey: string): void | any {
-    const originalSetter = Object.getOwnPropertyDescriptor(target, propertyKey)?.set;
-    let value: string;
-    Object.defineProperty(target, propertyKey, {
-      get() {
-        return value;
-      },
-      set(val: string) {
-        console.log('MinLength');
-        if (val.length < length) throw new Error(`Number of symbols can't be less than ${length}`);
-        originalSetter?.call(target, val);
-      },
-      configurable: true,
+interface IUpdateData {
+  name: string | undefined;
+  content: string | undefined;
+}
+
+class TodoList {
+  private tasks: Task[] = [];
+
+  addTask(data: IUpdateData): void {
+    const { name, content } = data;
+    if (!name || !content) throw new Error("Name or context can't be empty");
+
+    const task = new Task(this.tasks.length, name, content);
+    this.tasks.push(task);
+  }
+
+  deleteTask(taskId: Task['id']): void {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  editTask(taskId: Task['id'], data: IUpdateData): void {
+    this.getTask(taskId).edit(data);
+  }
+
+  setDone(taskId: Task['id']): void {
+    this.getTask(taskId).changeStatus();
+  }
+
+  getTask(taskId: Task['id']): Task {
+    return this.tasks.find(task => task.id === taskId);
+  }
+
+  getTasks(): (Task | ProtectedTask)[] {
+    return this.tasks;
+  }
+
+  getNumberOfAllTasks(): string {
+    return `There are ${this.getTasks().length} tasks`;
+  }
+
+  getNumberOfnotDoneTasks(): string {
+    return `There are ${this.tasks.filter(task => task.status).length} unfinished tasks`;
+  }
+}
+
+class ToDoListExtendingBehavior {
+  constructor(private readonly list: TodoList) { }
+
+  findTask(searchRequest: string): Task[] {
+    return this.list
+      .getTasks()
+      .filter(task => task.name.indexOf(searchRequest) >= 0 || task.content.indexOf(searchRequest) >= 0);
+  }
+
+  sortTasks(sort: Sort): Task[] {
+    return this.list.getTasks().sort((a, b) => {
+      if (sort === Sort.DATE) {
+        if (a.createdDate < b.createdDate) {
+          return -1;
+        }
+        if (a.createdDate > b.createdDate) {
+          return 1;
+        }
+      } else {
+        if (a.status < b.status) {
+          return -1;
+        }
+        if (a.status > b.status) {
+          return 1;
+        }
+      }
     });
-  };
-}
-
-function MaxLength(length: number) {
-  return function <T extends {}>(target: T, propertyKey: string): void | any {
-    const originalSetter = Object.getOwnPropertyDescriptor(target, propertyKey)?.set;
-    let value: string;
-
-    Object.defineProperty(target, propertyKey, {
-      get() {
-        return value;
-      },
-      set(val: string) {
-        console.log('MaxLength');
-        if (val.length > length) throw new Error(`Number of symbols can't be more than ${length}`);
-        originalSetter?.call(target, val);
-      },
-      configurable: true,
-    });
-  };
-}
-
-function Email<T extends {}>(target: T, propertyKey: string | symbol): void | any {
-  const originalSetter = Object.getOwnPropertyDescriptor(target, propertyKey)?.set;
-  let value: string;
-  Object.defineProperty(target, propertyKey, {
-    get() {
-      return value;
-    },
-    set(val: string) {
-
-      console.log('Email');
-      if (!/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/.test(val)) throw new Error('Email is not valid');
-      originalSetter?.call(target, val);
-    },
-    configurable: true,
-  });
-}
-
-function DeprecatedMethod(reason: string, replacedMethod?: string) {
-  return function <T, A extends any[], R>(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (this: T, ...args: A): R {
-      console.log(`Don't use this method because ${reason}${replacedMethod ? ', use ' + replacedMethod : ''}`);
-      return originalMethod.apply(this, args);
-    };
-  };
-}
-
-class Validation {
-  // @Email
-  // @MinLength(1)
-  // @MaxLength(5)
-  @Compose(1, 5)
-  email: string;
-
-  constructor(email: string) {
-    this.email = email;
-  }
-
-  @DeprecatedMethod('this Method is Old', 'setEmail')
-  setEmailDeprecated(value: string): void {
-    this.email = value;
-  }
-
-  setEmail(value: string): void {
-    this.email = value;
   }
 }
 
-const form = new Validation('mail@mail.com');
-form.email;
+class Task {
+  protected _createdDate: string;
+  protected editDate: string = undefined;
+  public status: boolean = false;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get content(): string {
+    return this._content;
+  }
+
+  get createdDate(): string {
+    return this._createdDate;
+  }
+
+  constructor(
+    public readonly id: number,
+    protected _name: string,
+    protected _content: string
+  ) {
+    this._createdDate = String(new Date());
+  }
+
+  edit(data: IUpdateData): void {
+    this.handleEdit(data);
+  }
+
+  changeStatus(): void {
+    this.status = !this.status;
+  }
+
+  protected handleEdit({ name, content }): void {
+    if (name) this._name = name;
+    if (content) this._content = content;
+    this.editDate = new Date().toString();
+  }
+}
+
+class ProtectedTask extends Task {
+  override edit(data: IUpdateData): void {
+    const confirmValue = confirm('Do you really want to change this task?');
+
+    if (confirmValue) {
+      this.handleEdit(data);
+    }
+  }
+}
