77df0f9d9a0506d9228afc95c9cbce5bb1d24cdd
Olha Kabatska <kabatskaya.olga@gmail.com>
2023-11-11 03:45:19 +0100

    add inizializator Zoo

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,41 @@ import { AnimalKindEnum, EmployeePositionsEnum, HealthEnum, NoticeTypeEnum, Tick
 import Employee from './entities/employee';
 import CashRegisterMediator from './mediators/cashRegisterMediator';
 
-const advertisingDepartment = new AdvertisingDepartment();
-const currentVisitors = new CurrentVisitors();
-const cashRegister = new CashRegister();
-const administration = new Administration(cashRegister, advertisingDepartment);
-const accounting = new Accounting(administration.employees, administration.animals);
+class Zoo {
+  public advertisingDepartment: AdvertisingDepartment;
+  public currentVisitors: CurrentVisitors;
+  public cashRegister: CashRegister;
+  public administration: Administration;
+  public accounting: Accounting;
 
-new CashRegisterMediator(cashRegister, currentVisitors, advertisingDepartment);
+  constructor() {
+    this.advertisingDepartment = new AdvertisingDepartment();
+    this.currentVisitors = new CurrentVisitors();
+    this.cashRegister = new CashRegister();
+    this.administration = new Administration(this.cashRegister, this.advertisingDepartment);
+    this.accounting = new Accounting(this.administration.employees, this.administration.animals);
+    new CashRegisterMediator(this.cashRegister, this.currentVisitors, this.advertisingDepartment);
+  }
+}
 
+const zoo = new Zoo();
 const fox = new Animal('fox1', 12, AnimalKindEnum.FOX, HealthEnum.GOOD, 200);
 const lion = new Animal('lion', 1, AnimalKindEnum.LION, HealthEnum.GOOD, 200);
-administration.addAnimal(fox);
-administration.addAnimal(lion);
+
+zoo.administration.addAnimal(fox);
+zoo.administration.addAnimal(lion);
 
 const feedingWorker = new Employee('John', 'Loh', EmployeePositionsEnum.ANIMAL_FEEDING_WORKER, 200);
 const accountant = new Employee('Loh', 'Ha', EmployeePositionsEnum.ACCOUNTANT, 300);
-administration.addEmployee(feedingWorker);
-administration.addEmployee(accountant);
 
-administration.createPrice(TicketTypeEnum.EDULT, 10);
-administration.createPrice(TicketTypeEnum.CHILD, 5);
-administration.createPrice(TicketTypeEnum.FAMILY, 20);
+zoo.administration.addEmployee(feedingWorker);
+zoo.administration.addEmployee(accountant);
 
-cashRegister.sellEdultTicket({
+zoo.administration.createPrice(TicketTypeEnum.EDULT, 10);
+zoo.administration.createPrice(TicketTypeEnum.CHILD, 5);
+zoo.administration.createPrice(TicketTypeEnum.FAMILY, 20);
+
+zoo.cashRegister.sellEdultTicket({
   firstName: 'LAla',
   lastName: 'Alal',
   phone: '2323234234',
@@ -38,9 +50,9 @@ cashRegister.sellEdultTicket({
 });
 
 for (let index = 0; index < 100; index++) {
-  cashRegister.sellChildTicket();
+  zoo.cashRegister.sellChildTicket();
 }
-cashRegister.sellFamilyTicket([
+zoo.cashRegister.sellFamilyTicket([
   {
     firstName: 'mom',
     lastName: 'mom',
@@ -55,16 +67,16 @@ cashRegister.sellFamilyTicket([
   },
 ]);
 
-advertisingDepartment.createEMailing('Wellcome, We are waiting for you');
+zoo.advertisingDepartment.createEMailing('Wellcome, We are waiting for you');
 
-currentVisitors.noticeClients(NoticeTypeEnum.ClosingIn15Minutes);
-currentVisitors.noticeClients(NoticeTypeEnum.Closed);
+zoo.currentVisitors.noticeClients(NoticeTypeEnum.ClosingIn15Minutes);
+zoo.currentVisitors.noticeClients(NoticeTypeEnum.Closed);
 
-accounting.paySalaries();
+zoo.accounting.paySalaries();
 
-accounting.getDayRevenue(cashRegister);
+zoo.accounting.getDayRevenue(zoo.cashRegister);
 
-accounting.createFinancialReport();
-accounting.createCostsForFeedingReport();
-accounting.createSalariesReport();
-administration.createAdvertision('Sale only Today');
+zoo.accounting.createFinancialReport();
+zoo.accounting.createCostsForFeedingReport();
+zoo.accounting.createSalariesReport();
+zoo.administration.createAdvertision('Sale only Today');
